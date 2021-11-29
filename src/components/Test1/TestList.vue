4cfc187365dcfbe4bdce464bb1ce4b25acb8b9c0
lintianhao <807696464@qq.com>
2021-11-29 17:38:28 +0800

    初始化 路由和几个页面

---
+++ src/components/Test1/TestList.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div v-if="dataList && dataList.length > 0">
+            <div v-for="(item,index) in dataList" :key="item.key">
+                name: {{item.name}} '|' index: {{index}} '|' 
+                <el-checkbox
+                    @change="val => changeChecked(val,index,item)"
+                    v-model="item.checked"
+                >
+                </el-checkbox>
+                
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'TestList',
+    data() {
+        return {
+            test1: 'test1',
+        }
+    },
+    props: {
+        dataList: {
+            type: Array,
+            default: [],
+        },
+    },
+    methods: {
+        changeChecked(val,index,item) {
+            console.log('list勾选改变',val,index,item.checked)
+            this.$nextTick(() => {
+                this.$emit('updateList',val,index,item)
+            })
+        }
+    }
+
+}
+</script>
---
+++ src/views/Test1/Test1Page2.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>test1page2</div>
+</template>
+
+<script>
+export default {
+    name: 'Test1Page2',
+}
+</script>
